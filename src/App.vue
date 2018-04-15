4d8f2868dd33fcaef34b656631ec1fd18d748b39
HE77XTL <he77505357@163.com>
2018-04-15 11:05:16 +0800

    单组件

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <He />
+    <he-comp1 />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import He from './components/He'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    He
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/He.vue
@@ -0,0 +1,9 @@
+<template>
+	<dl id="he">
+		<dt>姓名</dt>
+		<dd>东升</dd>
+		<dt>年龄</dt>
+		<dd>18</dd>
+	</dl>
+</template>
+
